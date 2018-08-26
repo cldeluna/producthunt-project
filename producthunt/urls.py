abf47bdf9589adbc9625f528f21bf42a40041bae
Claudia de Luna <claudia@indigowire.net>
2018-08-26 09:54:47 -0700

    Working Version

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/',include('accounts.urls')),
     path('acc/',include('accounts.urls')),
-    # path('products/', include('products.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/urls.py
+++ products/urls.py
@@ -2,8 +2,8 @@
 from django.urls import path, include
 from . import views
 
-# urlpatterns = [
-#     # path('create', views.create, name='create'),
-#     # path('<int:product_id>', views.detail, name='detail'),
-#     # path('<int:product_id>/upvote', views.upvote, name='upvote'),
-# ]
+urlpatterns = [
+    path('create/', views.create, name='create'),
+    path('<int:product_id>/', views.detail, name='detail'),
+    path('<int:product_id>/upvote/', views.upvote, name='upvote'),
+]
